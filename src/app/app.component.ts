dc44dad865e8162bafca4d93ab13de381e560178
Paolo <baioni2002@yahoo.fr>
2019-08-17 11:44:35 +0200

    setup firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularLibrary';
+    constructor() {
+        const config = {
+            apiKey: 'AIzaSyAOgNGdMpw1IPYN4nvIml6NSLGcSWF0K64',
+            authDomain: 'libraryoc-28a68.firebaseapp.com',
+            databaseURL: 'https://libraryoc-28a68.firebaseio.com',
+            projectId: 'libraryoc-28a68',
+            storageBucket: '',
+            messagingSenderId: '187755962455',
+            appId: '1:187755962455:web:72825443743e28b0'
+        };
+        firebase.initializeApp(config);
+    }
 }
